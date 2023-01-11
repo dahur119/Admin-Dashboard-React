e10e57a6e0cdc10a6d7fd6544d04c7758ea5245b
dayo <dahurdayo@example.com>
2023-01-11 11:54:13 +0100

    adminDashboard commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2 className="underline text-3xl">Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
